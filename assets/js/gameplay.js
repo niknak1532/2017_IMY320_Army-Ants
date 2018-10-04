37aa091555d0941342a3c3881f20e94bc9b0beef
LesegoGomolemo <lesegomabe@ymail.com>
2018-10-04 14:43:45 +0200

    Gameplay external styling and scripts

---
+++ assets/js/gameplay.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.gameplay_carousel').slick({
+      dots: true,
+      infinite: true,
+      speed: 500,
+      fade: true,
+      cssEase: 'linear'
+    });
+  });
